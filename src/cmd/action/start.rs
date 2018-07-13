92604a849e2e58ce5224daa3dd4048ec9f74e6e0
timvisee <timvisee@gmail.com>
2018-07-13 21:08:59 +0200

    Debug

--- src/cmd/action/start.rs
+++ src/cmd/action/start.rs
@@ -15,7 +15,7 @@ use telegram_bot::{
 use super::Action;
 
 /// The action command name.
-const CMD: &'static str = "start";
+const CMD: &'static str = "breakpoint";
 
 /// Whether the action is hidden.
 const HIDDEN: bool = true;
@@ -50,11 +50,8 @@ impl Action for Start {
         // Build a future for sending the response start message
         let future = api.send_timeout(
                 msg.text_reply(format!("\
-                            *Welcome {}!*\n\
-                            \n\
-                            To start using this bot, see the list of available commands by sending /help\
+                            LEKKER DEBUGGEN\
                         ",
-                        msg.from.first_name,
                     ))
                     .parse_mode(ParseMode::Markdown),
                 Duration::from_secs(10),
